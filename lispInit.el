b41d28ec2780fa55ab63a2a5a8dad533375d1e62
Ian Wahbe <ian@wahbe.com>
2019-02-10 16:31:36 -0800

    adding the lispinit file, currently very simmilar to the eLispInit file, I expect greater divergence as I spend more time learing lisp

---
+++ lispInit.el
@@ -0,0 +1,29 @@
+;;Loading Packages
+(require 'dracula-theme)
+(require 'slime)
+(require 'linum)
+(package-initialize)
+
+;;Visual Modification
+(load-theme 'dracula) ; to load the dracula theme
+(linum-mode) ; to enable linum mode
+(set-face-foreground 'linum "green")
+
+;;Real Modification
+(setq inferior-lisp-program "/usr/local/bin/clisp")
+(slime-mode t)
+(add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
+(slime-setup '(slime-fancy slime-company));(add-to-list 'company-backends 'slime-company)
+(slime) ; to turn on slime
+
+;; layout definition
+(defun my-startup-layout ()
+ (interactive)
+ (delete-other-windows)
+)
+
+;; execute the layout
+(my-startup-layout )
+
+
+
